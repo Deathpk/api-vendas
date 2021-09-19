adfb5cfe8b81c6cba646e7968960ecd27a85c253
Deathkpk <bettercallmiguel@gmail.com>
2021-09-19 20:45:16 -0300

    New - Products Migration and repository created.

---
+++ src/shared/typeorm/migrations/1632089369524-CreateProducts.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateProducts1632089369524 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'bigInt',
+            isPrimary: true,
+            generationStrategy: 'increment',
+          },
+
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+          },
+
+          {
+            name: 'quantity',
+            type: 'int',
+          },
+
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('products');
+  }
+}
