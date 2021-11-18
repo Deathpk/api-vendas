b4d1a805dfaae47f8eebd34d5ffd04a78835ba0b
unknown <bettercallmiguel@gmail.com>
2021-11-18 00:11:54 -0300

    New - Adding request validation using celebrate middleware

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,13 +1,56 @@
 import { Router } from 'express';
 import ProductsController from '@modules/products/controller/ProductsController';
-
+import { celebrate, Joi, Segments } from 'celebrate';
 const router = Router();
 const controller = new ProductsController();
 
 router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.delete('/:id', controller.delete);
+
+router.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.number().required(),
+    },
+  }),
+  controller.show,
+);
+
+router.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().min(3).max(120).required(),
+      price: Joi.number().required(),
+      quantity: Joi.number().min(1).required(),
+    },
+  }),
+  controller.create,
+);
+
+router.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.number().required(),
+    },
+    [Segments.BODY]: {
+      name: Joi.string().min(3).max(120).required(),
+      price: Joi.number().required(),
+      quantity: Joi.number().min(1).required(),
+    },
+  }),
+  controller.update,
+);
+
+router.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.number().required(),
+    },
+  }),
+  controller.delete,
+);
 
 export default router;
